4ca05bbc23319c1c6409103084670b276103727b
Mikel Granero <mikelgmh@gmail.com>
2020-11-05 16:53:34 +0100

    Añadidas dos excepciones más para comprobar si el usuario existe, el email existe o ambas existen

--- src/signupsignin/server/Worker.java
+++ src/signupsignin/server/Worker.java
@@ -1,11 +1,13 @@
 package signupsignin.server;
 
+import exceptions.EmailAlreadyExistsException;
 import exceptions.ErrorClosingDatabaseResources;
 import exceptions.ErrorConnectingDatabaseException;
 import exceptions.ErrorConnectingServerException;
 import exceptions.PasswordMissmatchException;
 import exceptions.QueryException;
 import exceptions.UserAlreadyExistException;
+import exceptions.UserAndEmailAlreadyExistException;
 import exceptions.UserNotFoundException;
 import interfaces.Signable;
 import java.util.logging.Level;
@@ -71,21 +73,28 @@ public class Worker extends Thread {
                  */
                 case SIGN_UP:
                     try {
-                        User user = dao.signUp(this.message.getUser());
-                        message = new Message(user, TypeMessage.REGISTER_OK);
-                    } catch (UserAlreadyExistException ex) {
-                        logger.log(Level.SEVERE, "The user already exist exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.USER_EXISTS);
-                    } catch (ErrorConnectingDatabaseException ex) {
-                        logger.log(Level.SEVERE, "Error connecting to the database exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.DATABASE_ERROR);
-                    } catch (QueryException ex) {
-                        logger.log(Level.SEVERE, "A query error exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.QUERY_ERROR);
-                    } catch (ErrorConnectingServerException ex) {
-                        Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    break;
+                    User user = dao.signUp(this.message.getUser());
+                    message = new Message(user, TypeMessage.REGISTER_OK);
+                } catch (UserAlreadyExistException ex) {
+                    logger.log(Level.SEVERE, "The user already exist exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.USER_EXISTS);
+                } catch (ErrorConnectingDatabaseException ex) {
+                    logger.log(Level.SEVERE, "Error connecting to the database exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.DATABASE_ERROR);
+                } catch (QueryException ex) {
+                    logger.log(Level.SEVERE, "A query error exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.QUERY_ERROR);
+                } catch (ErrorConnectingServerException ex) {
+                    Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (EmailAlreadyExistsException ex) {
+                    Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+                    message = new Message(this.message.getUser(), TypeMessage.EMAIL_EXISTS);
+                } catch (UserAndEmailAlreadyExistException ex) {
+                    Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+                    message = new Message(this.message.getUser(), TypeMessage.USER_AND_EMAIL_EXIST);
+                }
+                break;
+
                 /**
                  * In the other case we try to sign in, if it is all correct we
                  * send an ok message, if something goes wrong it catch the
@@ -93,36 +102,35 @@ public class Worker extends Thread {
                  */
                 case SIGN_IN:
                     try {
-                        User user = dao.signIn(this.message.getUser());
-                        message = new Message(user, TypeMessage.REGISTER_OK);
-                    } catch (ErrorConnectingDatabaseException ex) {
-                        logger.log(Level.SEVERE, "Error connecting to the database exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.DATABASE_ERROR);
-                    } catch (QueryException ex) {
-                        logger.log(Level.SEVERE, "A query error exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.QUERY_ERROR);
-                    } catch (UserNotFoundException ex) {
-                        logger.log(Level.SEVERE, "User not found exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.USER_DOES_NOT_EXIST);
-                    } catch (PasswordMissmatchException ex) {
-                        logger.log(Level.SEVERE, "The password does not match exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.LOGIN_ERROR);
-                    } catch (ErrorClosingDatabaseResources ex) {
-                        logger.log(Level.SEVERE, "Error closing the database resources exception ocurred.");
-                        message = new Message(this.message.getUser(), TypeMessage.STOP_SERVER);
-                    } catch (ErrorConnectingServerException ex) {
-                        Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    break;
+                    User user = dao.signIn(this.message.getUser());
+                    message = new Message(user, TypeMessage.REGISTER_OK);
+                } catch (ErrorConnectingDatabaseException ex) {
+                    logger.log(Level.SEVERE, "Error connecting to the database exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.DATABASE_ERROR);
+                } catch (QueryException ex) {
+                    logger.log(Level.SEVERE, "A query error exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.QUERY_ERROR);
+                } catch (UserNotFoundException ex) {
+                    logger.log(Level.SEVERE, "User not found exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.USER_DOES_NOT_EXIST);
+                } catch (PasswordMissmatchException ex) {
+                    logger.log(Level.SEVERE, "The password does not match exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.LOGIN_ERROR);
+                } catch (ErrorClosingDatabaseResources ex) {
+                    logger.log(Level.SEVERE, "Error closing the database resources exception ocurred.");
+                    message = new Message(this.message.getUser(), TypeMessage.STOP_SERVER);
+                } catch (ErrorConnectingServerException ex) {
+                    Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
 
             }
 
         } catch (IOException | ClassNotFoundException ex) {
             logger.log(Level.SEVERE, "Error trying to read the message.");
-        } 
-        /**
-         * In this finally clause the server return the message 
-         * and then it close the socket and the thread ends.
+        } /**
+         * In this finally clause the server return the message and then it
+         * close the socket and the thread ends.
          */
         finally {
             try {
--- src/signupsignin/server/dao/MySQLDaoImplementation.java
+++ src/signupsignin/server/dao/MySQLDaoImplementation.java
@@ -1,10 +1,12 @@
 package signupsignin.server.dao;
 
+import exceptions.EmailAlreadyExistsException;
 import exceptions.ErrorClosingDatabaseResources;
 import exceptions.ErrorConnectingDatabaseException;
 import exceptions.PasswordMissmatchException;
 import exceptions.QueryException;
 import exceptions.UserAlreadyExistException;
+import exceptions.UserAndEmailAlreadyExistException;
 import exceptions.UserNotFoundException;
 import interfaces.Signable;
 import java.sql.Connection;
@@ -17,6 +19,7 @@ import user.User;
 
 /**
  * It does the querys with the database.
+ *
  * @author Imanol, Mikel
  */
 public class MySQLDaoImplementation implements Signable {
@@ -29,13 +32,18 @@ public class MySQLDaoImplementation implements Signable {
     private final String checkPassword = "SELECT * FROM USER WHERE LOGIN=? AND PASSWORD=?";
     private final String insertAccess = "UPDATE USER SET LASTACCESS =? WHERE LOGIN=?";
     private final String checkIfUserExists = "SELECT * FROM USER WHERE LOGIN=? OR EMAIL=?";
+
     /**
-     * It does the querys to check if the login and the password are correct with the ones in the database.
+     * It does the querys to check if the login and the password are correct
+     * with the ones in the database.
+     *
      * @param user it contains the login, password data.
      * @return the user with the information that the database have returned.
-     * @throws ErrorConnectingDatabaseException if it doesn't connect with the database.
+     * @throws ErrorConnectingDatabaseException if it doesn't connect with the
+     * database.
      * @throws UserNotFoundException if an user doesn't exist in the database.
-     * @throws PasswordMissmatchException if the password is not the correct one for that user.
+     * @throws PasswordMissmatchException if the password is not the correct one
+     * for that user.
      * @throws ErrorClosingDatabaseResources if it fails closing the querys.
      * @throws QueryException when it execute the query it fails.
      */
@@ -77,16 +85,22 @@ public class MySQLDaoImplementation implements Signable {
         //Devuelvo user
         return user;
     }
+
     /**
-     * It does the check if user doesn't exist and the new user is added to the database.
+     * It does the check if user doesn't exist and the new user is added to the
+     * database.
+     *
      * @param user with data to register into the database.
      * @return user with the correct data.
      * @throws UserAlreadyExistException if the user exist in the database.
+     * @throws exceptions.UserAndEmailAlreadyExistException
+     * @throws exceptions.EmailAlreadyExistsException
      * @throws QueryException if the query fails.
-     * @throws ErrorConnectingDatabaseException if the connection to the database fails.
+     * @throws ErrorConnectingDatabaseException if the connection to the
+     * database fails.
      */
     @Override
-    public User signUp(User user) throws UserAlreadyExistException, QueryException, ErrorConnectingDatabaseException {
+    public User signUp(User user) throws UserAlreadyExistException,UserAndEmailAlreadyExistException, EmailAlreadyExistsException, QueryException, ErrorConnectingDatabaseException {
         try {
             this.con = ConnectionPool.getConnection();
             this.checkifUserExists(user);
@@ -114,13 +128,17 @@ public class MySQLDaoImplementation implements Signable {
         }
         return user;
     }
+
     /**
      * It checks if the user exists in the database.
+     *
      * @param user with data to register or login into the database.
-     * @throws SQLException if an error have happened with the query or the database.  
-     * @throws UserAlreadyExistException if the user already exist in the database.
+     * @throws SQLException if an error have happened with the query or the
+     * database.
+     * @throws UserAlreadyExistException if the user already exist in the
+     * database.
      */
-    private void checkifUserExists(User user) throws SQLException, UserAlreadyExistException {
+    private void checkifUserExists(User user) throws SQLException, UserAlreadyExistException, EmailAlreadyExistsException, UserAndEmailAlreadyExistException {
 
         this.ps = con.prepareStatement(this.checkIfUserExists);
         this.ps.setString(1, user.getLogin());
@@ -128,15 +146,25 @@ public class MySQLDaoImplementation implements Signable {
         this.rs = this.ps.executeQuery();
 
         while (rs.next()) {
-            throw new UserAlreadyExistException(user);
+            if (rs.getString("login").equalsIgnoreCase(user.getLogin()) && rs.getString("email").equalsIgnoreCase(user.getEmail())) {
+                throw new UserAndEmailAlreadyExistException();
+            } else if (rs.getString("login").equalsIgnoreCase(user.getLogin())) {
+                throw new UserAlreadyExistException(user);
+            } else if (rs.getString("email").equalsIgnoreCase(user.getEmail())) {
+                throw new EmailAlreadyExistsException();
+            }
+
         }
 
     }
+
     /**
      * It checks if the user is in the database.
+     *
      * @param user with data to register or login into the database.
      * @throws UserNotFoundException If the user is not found in the database.
-     * @throws SQLException If an error have happened with the query or the database. 
+     * @throws SQLException If an error have happened with the query or the
+     * database.
      */
     private void checkUser(User user) throws UserNotFoundException, SQLException {
         ps = con.prepareStatement(checkUser);
@@ -146,10 +174,13 @@ public class MySQLDaoImplementation implements Signable {
             throw new UserNotFoundException();
         }
     }
+
     /**
      * It is to change the last access of the user.
+     *
      * @param user with data to register or login into the database.
-     * @throws SQLException If an error have happened with the query or the database. 
+     * @throws SQLException If an error have happened with the query or the
+     * database.
      */
     private void insertAccesTime(User user) throws SQLException {
         java.sql.Timestamp date = new java.sql.Timestamp(new java.util.Date().getTime());
@@ -159,9 +190,12 @@ public class MySQLDaoImplementation implements Signable {
         ps.executeUpdate();
         ps.close();
     }
+
     /**
      * It close the connection.
-     * @throws SQLException if an error have happened with the query or the database. 
+     *
+     * @throws SQLException if an error have happened with the query or the
+     * database.
      */
     private void closeConnection() throws SQLException {
         if (this.rs != null) {
